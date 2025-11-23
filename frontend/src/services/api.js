95a5082f2478b3106f2bb08ec9b2486da28471a7
Vanbandon <theo.vanbandon@gmail.com>
2025-11-24 00:20:07 +0100

    Changement concernant le déploiement du back

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
  * Service d'appels API au backend
  */
 
-const API_BASE_URL = "http://localhost:4000/api";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:4000/api";
 
 /**
  * Génère une spéciale de rallye
